bb1224cc42dca3affbc0a593d4f9b920fbe06c33
adjivas <adjivas@users.noreply.github.com>
2017-10-22 11:45:05 +0200

    fixe path tcl-core to tcl

--- script/5.11.1.sh
+++ script/5.11.1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 5.11.1. Installation of Tcl-core 
 cd $LFS/sources
-tar -xf tcl-core8.6.7-src.tar.gz && cd Tcl-core-8.6.7-src
+tar -xf tcl-core8.6.7-src.tar.gz && cd tcl-8.6.7
 
 ## Prepare Tcl for compilation:
 cd unix
@@ -25,4 +25,4 @@ make install-private-headers
 ## Now make a necessary symbolic link:
 ln -sv tclsh8.6 /tools/bin/tclsh
 
-cd $LFS/sources && rm -rf tclsh8.6
+cd $LFS/sources && rm -rf tclsh8.6.7
