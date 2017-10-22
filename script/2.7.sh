77b8aac14e892385c751dc1cc68a0813aed66414
adjivas <adjivas@users.noreply.github.com>
2017-10-22 07:41:50 +0200

    auto-mount pre-fixe

--- script/2.7.sh
+++ script/2.7.sh
@@ -2,13 +2,13 @@
 
 ## Create the mount point and mount the LFS file system by running:
 mkdir -pv $LFS && mount -v -t ext3 /dev/sdb2 $LFS
-mkdir -v $LFS/home && mount -v -t ext3 /dev/sdb3 $LFS/home
-mkdir -v $LFS/boot && mount -v -t ext2 /dev/sdb1 $LFS/boot
+mkdir -pv $LFS/home && mount -v -t ext3 /dev/sdb3 $LFS/home
+mkdir -pv $LFS/boot && mount -v -t ext2 /dev/sdb1 $LFS/boot
 
-mkdir -v $LFS/opt && mount -v -t ext3 /dev/sdb6 $LFS/opt
-mkdir -v $LFS/tmp && mount -v -t ext3 /dev/sdb8 $LFS/tmp
-mkdir -v $LFS/usr && mount -v -t ext3 /dev/sdb7 $LFS/usr
-mkdir -v $LFS/usr/src && mount -v -t ext4 /dev/sdb5 $LFS/usr/src
+mkdir -pv $LFS/opt && mount -v -t ext3 /dev/sdb6 $LFS/opt
+mkdir -pv $LFS/tmp && mount -v -t ext3 /dev/sdb8 $LFS/tmp
+mkdir -pv $LFS/usr && mount -v -t ext3 /dev/sdb7 $LFS/usr
+mkdir -pv $LFS/usr/src && mount -v -t ext4 /dev/sdb5 $LFS/usr/src
 
 echo "/dev/sdb2     /mnt/lfs            ext3    defaults            0     1" >> /etc/fstab
 echo "/dev/sdb6     /mnt/lfs/opt        ext3    defaults            0     1" >> /etc/fstab
